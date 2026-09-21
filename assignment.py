faef2d457954cacb7b4fc72cdf8a207674bb10a3
Enereldalai <endii0428@gmail.com>
2026-09-21 09:26:29 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -48,16 +48,8 @@ def count_words(text):
 
 # Exercise 6
 def find_longest_word(text):
-    s=0
-    h=0
-    for char in text:
-        if char!=' ':
-            s+=char
-        else:
-            if h<s:
-                h=s
-            s=""
-    if len(h)<len(s):
-        h = s
-    return h
+    s=text.split()
+    f=max(s,key=len)
+    return f
     pass
+print(find_longest_word("tehas agsgaas"))
